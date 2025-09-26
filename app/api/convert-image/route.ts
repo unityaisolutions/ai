e6933607d62aeeaa97cf001b52600c559fa7c4cc
unityaisolutions <unityaisolutions@outlook.com>
2025-09-26 17:47:47 +0000

    Update image conversion function to run on Node.js runtime.

--- app/api/convert-image/route.ts
+++ app/api/convert-image/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { put } from '@vercel/blob';
 import sharp from 'sharp';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
